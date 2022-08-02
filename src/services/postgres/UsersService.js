e17e484ee2a2a02ef4493d5d7c08500c02172c8c
Fadhil423 <fadhil29172@gmail.com>
2022-08-02 21:55:54 +0700

    collaboration implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -13,26 +13,21 @@ class UsersService {
     this._pool = new Pool();
   }
 
-  async verifyUserCredential(username, password) {
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
-      text: 'SELECT id, password FROM users WHERE username = $1',
-      values: [username],
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
     };
 
     const result = await this._pool.query(query);
+
     if (!result.rows.length) {
-      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+      throw new InvariantError('User gagal ditambahkan');
     }
-
-    const { id, password: hashedPassword } = result.rows[0];
-
-    const match = await bcrypt.compare(password, hashedPassword);
-
-    if (!match) {
-      throw new AuthenticationError('Kredensial yang Anda berikan salah');
-    }
-
-    return id;
+    return result.rows[0].id;
   }
 
   async verifyNewUsername(username) {
@@ -48,23 +43,6 @@ class UsersService {
     }
   }
 
-  async addUser({ username, password, fullname }) {
-    await this.verifyNewUsername(username);
-
-    const id = `user-${nanoid(16)}`;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const query = {
-      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
-      values: [id, username, hashedPassword, fullname],
-    };
-
-    const result = await this._pool.query(query);
-    if (!result.rows.length) {
-      throw new InvariantError('User gagal ditambahkan');
-    }
-    return result.rows[0].id;
-  }
-
   async getUserById(userId) {
     const query = {
       text: 'SELECT id, username, fullname FROM users WHERE id = $1',
@@ -78,6 +56,38 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
   }
 }
 
