2c17dc26056e4e965ae04436256534ad68dd4b30
Fadhil423 <fadhil29172@gmail.com>
2022-08-21 16:23:54 +0700

    local storage implemented

---
+++ src/validator/uploads/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const InvariantError = require('../../exceptions/InvariantError');
+const { ImageHeaderSchema } = require('./schema');
+
+const UploadsValidator = {
+  validateImageHeaders: (headers) => {
+    const validationResult = ImageHeaderSchema.validate(headers);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UploadsValidator;
